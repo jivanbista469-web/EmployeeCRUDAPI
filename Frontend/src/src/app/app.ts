418a4605b279c8be3e6449005ea4aa11e6d034b8
User <User@DESKTOP-N13U4K3>
2026-09-21 08:29:48 +0545

    added initial bootstrap

--- Frontend/src/src/app/app.ts
+++ Frontend/src/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbCollapse],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('employeecrud');
+  protected readonly title = signal('Employee Management');
+  protected employeeCollapsed = true;
 }
