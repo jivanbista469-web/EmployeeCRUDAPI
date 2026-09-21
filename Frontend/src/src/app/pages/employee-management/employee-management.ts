e9a4760953d24b1a521da9e09520dde9c956e071
User <User@DESKTOP-N13U4K3>
2026-09-21 13:14:20 +0545

    added employee component

---
+++ Frontend/src/src/app/pages/employee-management/employee-management.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-employee-management',
+  styleUrl: './employee-management.scss',
+  templateUrl: './employee-management.html',
+})
+export class EmployeeManagement {
+  title: string = 'Employee Management';
+}
